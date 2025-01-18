4ed02a8d234baeffe745ee92d09436b5630cdae3
BugHunter-X <askzenai@gmail.com>
2025-01-18 09:56:04 +0100

    Add bug.rs file

---
+++ bug.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    let index = 5;
+
+    // This will panic at runtime if index is out of bounds
+    let value = vec[index];
+    println!("Value at index {}: {}", index, value);
+}
